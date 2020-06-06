6cd1ec7d310f5011ea5d4b0ca7e92a340b1ae532
Tedhoon <gt0305@likelion.org>
2020-06-06 14:58:01 +0900

    [deploy] react build

---
+++ react/quizfront/build/precache-manifest.f79984b0c401cfea6476095267299198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7752c907a39afb94ee07d2329e2699ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "118f85b7acb83a2e851b",
+    "url": "./static/js/2.86b0f844.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "./static/js/2.86b0f844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689778a6f924d5975b21",
+    "url": "./static/js/main.d46775c7.chunk.js"
+  },
+  {
+    "revision": "ed0f4f86d22ddcee862f",
+    "url": "./static/js/runtime-main.0df79b1c.js"
+  },
+  {
+    "revision": "638b8471d8607c64142f58f1eac6f604",
+    "url": "./static/media/DungGeunMo.638b8471.ttf"
+  },
+  {
+    "revision": "248b151ec333bb72c87a54530637822c",
+    "url": "./static/media/ebc.248b151e.jpg"
+  }
+]);
