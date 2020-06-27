37fceabe9a9f172638f20ac63ca0237097efdebe
Tedhoon <gt0305@likelion.org>
2020-06-27 12:00:31 +0900

    [add] result api connection

--- react/quizfront/src/Pages/ReNewResultPage.js
+++ react/quizfront/src/Pages/ReNewResultPage.js
@@ -1,60 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import ResultData from 'Data/Result/ResultData';
 import styled from 'styled-components';
+import axios from 'axios';
+import { BACKEND } from 'config';
 
 function ReNewResultPage() {
     const history = useHistory();
-    const resultData = ResultData;
-    const [score, setScore] = useState(null);
-    const [category, setCategory] = useState(null);
+    const [resultData, setResultData] = useState(null);
     const location = useLocation();
-    
-    useEffect(()=>{
-        setTimeout(()=>{
-            window.Kakao.init('3fabadf710529806099b2db7ea2d0041');
-            window.Kakao.Link.createCustomButton({
-            container: '#kakao-link-btn',
-            templateId: 29775,
-            templateArgs: {
-              'title': '1일 7깡 테스트',
-              'description': '나는 1일 몇 깡일까? spnu.net',
-              'url': 'http://spnu.net'
-            }
-            });
-        },300)        
+
+    // useEffect(()=>{
+    //     setTimeout(()=>{
+    //         window.Kakao.init('3fabadf710529806099b2db7ea2d0041');
+    //         window.Kakao.Link.createCustomButton({
+    //         container: '#kakao-link-btn',
+    //         templateId: 29775,
+    //         templateArgs: {
+    //           'title': '1일 7깡 테스트',
+    //           'description': '나는 1일 몇 깡일까? spnu.net',
+    //           'url': 'http://spnu.net'
+    //         }
+    //         });
+    //     },300)        
+    // },[])
+
+    const mockAsyncResultData = (score, category_pk) => 
+        new Promise(resolve => {
+            setTimeout(async () => {
+                const result = await axios.get(`${BACKEND}/api/result/${score}/${category_pk}/`)
+                resolve({
+                    data: result.data[0]
+                })
+            }, 200)
+        })
+        
+
+    const getResultData = useCallback(async (score, {id: category_pk}) =>{
+        try {
+            const { data } = await mockAsyncResultData(score, category_pk);
+            setResultData(data)
+        } catch (err) {
+            console.log(err);
+        }
     },[])
-    
+
+
     useEffect(()=>{
-        setTimeout(()=>{
-            console.log(location.state.category);
-            console.log(location)
-            console.log(location.state)
-            console.log(location.state.score)
-            setScore(location.state.score);
+        setTimeout(async ()=>{
+            const { score, category } = await location.state
+            getResultData(score, category[0])
         },250)
     },[]) 
     
+    useEffect(()=>{
+        console.log(resultData)
+    },[resultData])
+
     const handleGoBack = () => (
         history.push("/")
     )
 
     return ( 
         <ResultWrapper>
-            {score !== null?
-                <>
-                    <ResultImg src={resultData[score].img} />
-                    <Result>
-                        {resultData[score].grade}
-                        <ResultScore>[{score}/5]</ResultScore>
-                    </Result>
-                    
-                    <ResultInfo>{resultData[score].info}</ResultInfo>
-                    <ReStartBtn onClick={handleGoBack}>다시하기</ReStartBtn>
-                    
-                    <Share>
+            {resultData?
+                <>  
+                    {BACKEND}
+                    {resultData['thumbnail']}
+
+
+                    <ResultImg src="" />
+                    {/* <Share>
                     <div id="kakao-link-btn" className="nes-btn is-warning">카톡으로 공유하기</div>
-                    </Share>
+                    </Share> */}
                 </> 
                 :
                 ''
